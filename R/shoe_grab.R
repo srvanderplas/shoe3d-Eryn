b08ea555978132659b736a138bebb2df0a6ab420
Eryn B <eblagg@iastate.edu>
2019-06-27 14:23:03 -0500

    Adding some assertions to the shoe_grab function

--- R/shoe_grab.R
+++ R/shoe_grab.R
@@ -14,30 +14,37 @@
 #'
 #' @importFrom Rvcg
 #' @importFrom stringr
+#' @import assertthat
 
 
 
 
-
-
-
-shoe_grab<- function(shoeid,scandate, filepath){
+shoe_grab<- function(shoeid,scandate=NULL, filepath){
   #function input checks
 
 
-  #setting up the file path to the data. This assumes that the file you want is in the folder labled data-raw
-  #this will only work on study shoes
+  #setting up the file path to the data.
 stl_files <- list.files( filepath, pattern = ".stl", full.names = T)
+        #checking to see if the folder containing the data is not empty
+          assertthat::not_empty(stl_files)
+
 
   #grabbing a single shoe
 shoe_paths <- stl_files[str_detect(stl_files, shoeid)]
-shoe_path<-shoe_paths[str_detect(shoe_paths, "3_1_1")]
-shoe<-shoe_path[str_detect(shoe_path, scandate)]
+shoePath<-shoe_paths[str_detect(shoe_paths, "3_1_1")]
+shoe<-shoePath[str_detect(shoePath, scandate)]
+
+        #checking to see if the file exists
+          assertthat::see_if(file.exists(shoe))
+
 
 #turning it into a mesh object
-shoe_mesh <- vcgImport(shoe, clean = T)
+shoe_mesh <- Rvcg::vcgImport(shoe, clean = T)
+        #Checking that it is a mesh3d object
+          assertthat::assert_that(class(shoe_mesh)=="mesh3d")
 
 
+#returning a mesh3d object
 return(shoe_mesh)
 
 }
