c4f872b4ae79d50b302ad48e3e0d05aea3d70eff
Eryn B <eblagg@iastate.edu>
2019-07-12 10:30:39 -0500

    adding some alignment issues

--- R/shoe_grab.R
+++ R/shoe_grab.R
@@ -24,27 +24,43 @@ shoe_grab<- function(shoeid,scandate=NULL, filepath){
 
 
   #setting up the file path to the data.
-stl_files <- list.files( filepath, pattern = ".stl", full.names = T)
+stl_files <- list.files( filepath, pattern = paste0(shoeid, ".*.stl"), full.names = T)
         #checking to see if the folder containing the data is not empty
           assertthat::not_empty(stl_files)
 
+if(!is.null(scandate)){
 
-  #grabbing a single shoe
-shoe_paths <- stl_files[str_detect(stl_files, shoeid)]
-shoePath<-shoe_paths[str_detect(shoe_paths, "3_1_1")]
-shoe<-shoePath[str_detect(shoePath, scandate)]
+    #grabbing a single shoe
+  shoePath<-stl_files[str_detect(stl_files, "3_1_1")]
+  shoe<-shoePath[str_detect(shoePath, scandate)]
 
         #checking to see if the file exists
           assertthat::see_if(file.exists(shoe))
 
 
-#turning it into a mesh object
-shoe_mesh <- Rvcg::vcgImport(shoe, clean = T)
+  #turning it into a mesh object
+  shoe_mesh <- Rvcg::vcgImport(shoe, clean = T)
         #Checking that it is a mesh3d object
           assertthat::assert_that(class(shoe_mesh)=="mesh3d")
 
+}
+
+if(is.null(scandate)) {
+  shoespath<-stl_files[str_detect(stl_files, "3_1_1")]
+
+  shoe_mesh <- tibble(Shoe = shoespath) %>%
+    mutate(exists = file.exists(shoespath),
+           stl = purrr::map(Shoe, Rvcg::vcgImport))
+
+}
+
+
 
 #returning a mesh3d object
-return(shoe_mesh)
+return(list(shoe_mesh))
+
+
+
+
 
 }
