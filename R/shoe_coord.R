5ffaf0ffb27fe72044d754cfaa2d0834baac8160
Eryn B <eblagg@iastate.edu>
2019-09-16 12:20:49 -0500

    Adding checks to coord function

--- R/shoe_coord.R
+++ R/shoe_coord.R
@@ -18,8 +18,11 @@
 
 shoe_coord<-function(shoe, verts=NULL){
 
+centeredshoe<-Arothron::trasf.mesh(shoe,barycenter = Arothron::bary.mesh(shoe))
+
+#try angles and allign it
 if(!is.null(verts)){
-vert<-as.data.frame(t(shoe$it)) %>%
+vert<-as.data.frame(t(centeredshoe$it)) %>%
   mutate(triangle_id = 1:n()) %>%
   tidyr::gather(-triangle_id, key = "vertex", value = idx) %>%
   group_by(idx)  %>%
@@ -29,21 +32,20 @@ vert<-as.data.frame(t(shoe$it)) %>%
   dplyr::filter(!is.na(V1) & !is.na(V2) & !is.na(V3))}
 
 if(is.null(verts))  {
-  vert<-as.data.frame(t(shoe$it)) %>%
+  vert<-as.data.frame(t(centeredshoe$it)) %>%
     mutate(triangle_id = 1:n()) %>%
     tidyr::gather(-triangle_id, key = "vertex", value = idx) %>%
     group_by(idx)  %>%
     ungroup(idx)%>%
     tidyr::spread(key="vertex", value=idx) %>%
     dplyr::filter(!is.na(V1) & !is.na(V2) & !is.na(V3))}
-}
 
 vert_long <- vert %>%
   select(-triangle_id) %>%
   tidyr::gather(key = V, value = idx) %>%
   select(-V) %>%
   unique()
-vert_coords <- shoe %>%
+vert_coords <- centeredshoe %>%
   magrittr::extract2("vb") %>%
   t() %>%
   magrittr::set_colnames(c("x", "y", "z", "idk")) %>%
@@ -53,6 +55,7 @@ x <- left_join(vert_long, vert_coords)%>% select(-idk)%>% select(-idx)
 
 hul<-geometry::convhulln(x, output.options = c("p", "Fx"),
                          return.non.triangulated.facets = FALSE)
+#flatten shoe here with old code from origional
 
 hull<-as.vector(hul)%>%unique()%>%as.data.frame()
 colnames(hull) <- "idx"
@@ -62,4 +65,5 @@ edge_coords<-vert_coords%>%
   select(-idk)%>%select(-idx)%>%as.matrix()
 
 return(edge_coords)
+
 }
