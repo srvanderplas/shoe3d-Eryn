931195670ac24ea1ee32ec37edd07972ae467394
Susan Vanderplas <srvanderplas@gmail.com>
2019-10-29 08:51:32 -0500

    Styler code fixing

--- R/shoe_coord.R
+++ R/shoe_coord.R
@@ -17,71 +17,80 @@
 #' @importFrom Arothron
 #' @import assertthat
 
-shoe_coord<-function(shoe, verts=NULL){
-  #check imputs
+shoe_coord <- function(shoe, verts = NULL) {
+  # check imputs
   assertthat::not_empty(shoe)
-  assertthat::assert_that(class(shoe)=="mesh3d")
+  assertthat::assert_that(class(shoe) == "mesh3d")
   assertthat::is.number(verts)
 
-#centering the shoe based on the center of mass
-centeredshoe<-Arothron::trasf.mesh(shoe,barycenter = Arothron::bary.mesh(shoe))
+  # centering the shoe based on the center of mass
+  centeredshoe <- Arothron::trasf.mesh(shoe, barycenter = Arothron::bary.mesh(shoe))
 
-  assertthat::assert_that(class(centeredshoe)=="mesh3d")
-
-
-
-if(!is.null(verts)){
-vert<-as.data.frame(t(centeredshoe$it)) %>%
-  mutate(triangle_id = 1:n()) %>%
-  tidyr::gather(-triangle_id, key = "vertex", value = idx) %>%
-  group_by(idx)  %>%
-  dplyr::filter(n() <= verts)%>%
-  ungroup(idx)%>%
-  tidyr::spread(key="vertex", value=idx) %>%
-  dplyr::filter(!is.na(V1) & !is.na(V2) & !is.na(V3))}
-
-
-if(is.null(verts))  {
-  vert<-as.data.frame(t(centeredshoe$it)) %>%
-    mutate(triangle_id = 1:n()) %>%
-    tidyr::gather(-triangle_id, key = "vertex", value = idx) %>%
-    group_by(idx)  %>%
-    ungroup(idx)%>%
-    tidyr::spread(key="vertex", value=idx) %>%
-    dplyr::filter(!is.na(V1) & !is.na(V2) & !is.na(V3))}
-#checking
-assertthat::not_empty(verts)
-
-
-vert_long <- vert %>%
-  select(-triangle_id) %>%
-  tidyr::gather(key = V, value = idx) %>%
-  select(-V) %>%
-  unique()
-vert_coords <- centeredshoe %>%
-  magrittr::extract2("vb") %>%
-  t() %>%
-  magrittr::set_colnames(c("x", "y", "z", "idk")) %>%
-  as_tibble() %>%
-  mutate(idx = 1:n())
-
-
-x <- left_join(vert_long, vert_coords)%>% select(-idk)%>% select(-idx)
-
-hul<-geometry::convhulln(x, output.options = c("p", "Fx"),
-                         return.non.triangulated.facets = FALSE)
+  assertthat::assert_that(class(centeredshoe) == "mesh3d")
 
 
 
-hull<-as.vector(hul)%>%unique()%>%as.data.frame()
-colnames(hull) <- "idx"
+  if (!is.null(verts)) {
+    vert <- as.data.frame(t(centeredshoe$it)) %>%
+      mutate(triangle_id = 1:n()) %>%
+      tidyr::gather(-triangle_id, key = "vertex", value = idx) %>%
+      group_by(idx) %>%
+      dplyr::filter(n() <= verts) %>%
+      ungroup(idx) %>%
+      tidyr::spread(key = "vertex", value = idx) %>%
+      dplyr::filter(!is.na(V1) & !is.na(V2) & !is.na(V3))
+  }
 
-edge_coords<-vert_coords%>%
-  dplyr::filter(idx %in% hull$idx)%>%
-  select(-idk)%>%select(-idx)%>%as.matrix()
 
-assertthat::assert_that(class(edge_coords)== "matrix")
+  if (is.null(verts)) {
+    vert <- as.data.frame(t(centeredshoe$it)) %>%
+      mutate(triangle_id = 1:n()) %>%
+      tidyr::gather(-triangle_id, key = "vertex", value = idx) %>%
+      group_by(idx) %>%
+      ungroup(idx) %>%
+      tidyr::spread(key = "vertex", value = idx) %>%
+      dplyr::filter(!is.na(V1) & !is.na(V2) & !is.na(V3))
+  }
+  # checking
+  assertthat::not_empty(verts)
 
-return(edge_coords)
 
+  vert_long <- vert %>%
+    select(-triangle_id) %>%
+    tidyr::gather(key = V, value = idx) %>%
+    select(-V) %>%
+    unique()
+  vert_coords <- centeredshoe %>%
+    magrittr::extract2("vb") %>%
+    t() %>%
+    magrittr::set_colnames(c("x", "y", "z", "idk")) %>%
+    as_tibble() %>%
+    mutate(idx = 1:n())
+
+
+  x <- left_join(vert_long, vert_coords) %>%
+    select(-idk) %>%
+    select(-idx)
+
+  hul <- geometry::convhulln(x,
+    output.options = c("p", "Fx"),
+    return.non.triangulated.facets = FALSE
+  )
+
+
+
+  hull <- as.vector(hul) %>%
+    unique() %>%
+    as.data.frame()
+  colnames(hull) <- "idx"
+
+  edge_coords <- vert_coords %>%
+    dplyr::filter(idx %in% hull$idx) %>%
+    select(-idk) %>%
+    select(-idx) %>%
+    as.matrix()
+
+  assertthat::assert_that(class(edge_coords) == "matrix")
+
+  return(edge_coords)
 }
--- R/shoe_grab.R
+++ R/shoe_grab.R
@@ -22,37 +22,36 @@
 shoe_grab <- function(shoeid, scandate = NULL, filepath) {
   # function input checks
 
-  #setting up the file path to the data.
-  stl_files <- list.files( filepath, pattern = paste0(shoeid, ".*.stl"), full.names = T)
-  #checking to see if the folder containing the data is not empty
+  # setting up the file path to the data.
+  stl_files <- list.files(filepath, pattern = paste0(shoeid, ".*.stl"), full.names = T)
+  # checking to see if the folder containing the data is not empty
   assertthat::not_empty(stl_files)
 
-  if(!is.null(scandate)){
+  if (!is.null(scandate)) {
 
-    #grabbing a single shoe
-    shoePath<-stl_files[str_detect(stl_files, "3_1_1")]
-    shoe<-shoePath[str_detect(shoePath, scandate)]
+    # grabbing a single shoe
+    shoePath <- stl_files[str_detect(stl_files, "3_1_1")]
+    shoe <- shoePath[str_detect(shoePath, scandate)]
 
     # checking to see if the file exists
     assertthat::see_if(file.exists(shoe))
 
-    #turning it into a mesh object
+    # turning it into a mesh object
     shoe_mesh <- Rvcg::vcgImport(shoe, clean = T)
-    #Checking that it is a mesh3d object
-    assertthat::assert_that(class(shoe_mesh)=="mesh3d")
-
+    # Checking that it is a mesh3d object
+    assertthat::assert_that(class(shoe_mesh) == "mesh3d")
   }
 
-  if(is.null(scandate)) {
-    shoespath<-stl_files[str_detect(stl_files, "3_1_1")]
+  if (is.null(scandate)) {
+    shoespath <- stl_files[str_detect(stl_files, "3_1_1")]
 
     shoe_mesh <- tibble(Shoe = shoespath) %>%
-      mutate(exists = file.exists(shoespath),
-             stl = purrr::map(Shoe, Rvcg::vcgImport))
-
+      mutate(
+        exists = file.exists(shoespath),
+        stl = purrr::map(Shoe, Rvcg::vcgImport)
+      )
   }
 
-  #returning a mesh3d object
+  # returning a mesh3d object
   return(list(shoe_mesh))
-
 }
