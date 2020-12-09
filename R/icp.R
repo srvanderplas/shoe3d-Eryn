3adc6e14275172c0d770707807158eb0c8c324d2
Eryn Blagg <eblagg@iastate.edu>
2020-12-09 10:39:40 -0600

    changes

--- R/icp.R
+++ R/icp.R
@@ -1,3 +1,7 @@
+
+
+
+
 icp <- function(mesh1, mesh2, iterations=3,lm1=NULL, lm2=NULL, uprange=1, maxdist=NULL, minclost=50, distinc=0.5, rhotol=pi, k=50, reflection=FALSE, silent=FALSE,subsample=NULL,subsampletype=c("km","pd"),type=c("rigid","similarity","affine"),getTransform=FALSE,pcAlign=FALSE,pcOptim=TRUE,threads=0,weights=NULL) {
 
   meshorig <- mesh1 <- vcgUpdateNormals(mesh1,silent=silent)
